a4271d3eba44fa28a12502dfda2f0f314a4a5cce
charles amper <camper.dev@gmail.com>
2016-06-26 16:01:48 +0800

    init commit

--- src/main/java/org/amperca/myapp/model/Authorities.java
+++ src/main/java/org/amperca/myapp/model/Authorities.java
@@ -1,8 +1,10 @@
 package org.amperca.myapp.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Entity
 public class Authorities {
 	
 	@Id
